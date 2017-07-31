a87b6f3461aa38f47bcbcc436bf280b7a68317c7
d-beloved <morayodeji@gmail.com>
2017-07-31 16:01:42 +0100

    The source code for my aritGeo app works well now

--- src/aritGeo.js
+++ src/aritGeo.js
@@ -2,33 +2,38 @@
 //whole script strict mode syntax
 'use strict';
 
-module.exports = {
-
 /*the codes below determiines if an array entered is
 an arithmetic progressiion of geometric progression
 or if it is none of the two progressions*/
 
-aritGeo: (array) => {
+module.exports = function aritGeo(array) {
 
 //checks if the entered value is an array, if it is not, it throws error
 	if (Array.isArray(array) === false) {
-		let error = "invalid input";
+		let error = "Invalid Input";
 		return error;
 	}
 //checks if the array is an empty array
-	else if (array === []) {
+	else if (array.length === 0) {
 		return 0;
 	}
-//checks if the array contains non-numbers by looping through the array an returning the output
-	else if (
-		for (let i = 0; i >= array.length; i++) {
-			if (typeof array[i] === "string") {
-			var error = "Invalid"; 
-		} 
-	}) { return error; }
 
+//implementing the formula for arithmetic progression a_n = a + (n-1)d
+//where a_n is the nth term, a is the first elememnt of the array
+//n is the number of the array, d is the common difference
+	else if (array[array.length-1] === array[0] + (array.length - 1)*(array[1] - array[0]) ) {
+		return "Arithmetic";
+	}
+//implementing the formula for geometric progression a_n = a*math.pow(r, n-1)
+//where a_n is the nth element of the array, a is the first element of the array
+//r is the common ratio of the elements of the array.
 
-}
+	else if (array[array.length-1] === array[0] * Math.pow(array[2]/array[1], array.length - 1)) {
+		return "Geometric";
+	}
 
+	else {
+		return -1;
+	}
 
 }
