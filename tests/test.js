48ab95ae33eb1aca92d992a38344bd72da6f94ee
d-beloved <morayodeji@gmail.com>
2017-07-31 14:02:54 +0100

    Just a minor editing of the test case, basically a typographical error correction

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@
 
   describe("Determine what the sequence an array of numbers is: ", function() {
 
-    describe("Ccheck if the argument is an array", function() {
+    describe("Check if the argument is an array", function() {
 
       it("should return Invalid input for a non array", function() {
         assert.equal(aritGeo("Any other thing")).toEqual("Invalid Input");
