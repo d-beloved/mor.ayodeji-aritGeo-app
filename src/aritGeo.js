ee55cdf21f90a76ba0186e293ae6d88182fda7ae
d-beloved <morayodeji@gmail.com>
2017-07-31 14:01:25 +0100

    The code for the aritGeo app is being written

--- src/aritGeo.js
+++ src/aritGeo.js
@@ -10,7 +10,22 @@ or if it is none of the two progressions*/
 
 aritGeo: (array) => {
 
-
+//checks if the entered value is an array, if it is not, it throws error
+	if (Array.isArray(array) === false) {
+		let error = "invalid input";
+		return error;
+	}
+//checks if the array is an empty array
+	else if (array === []) {
+		return 0;
+	}
+//checks if the array contains non-numbers by looping through the array an returning the output
+	else if (
+		for (let i = 0; i >= array.length; i++) {
+			if (typeof array[i] === "string") {
+			var error = "Invalid"; 
+		} 
+	}) { return error; }
 
 
 }
